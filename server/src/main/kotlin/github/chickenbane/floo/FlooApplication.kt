ecb6228cb55af09c93a2cec45a05ac07b582b3bd
Joey T <thechickenbane@gmail.com>
2018-05-26 11:09:23 -0700

    initial protobuf + grpc server

---
+++ server/src/main/kotlin/github/chickenbane/floo/FlooApplication.kt
@@ -0,0 +1,23 @@
+package github.chickenbane.floo
+
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.shell.standard.ShellComponent
+import org.springframework.shell.standard.ShellMethod
+
+@SpringBootApplication
+class FlooApplication
+
+fun main(args: Array<String>) {
+    runApplication<FlooApplication>(*args)
+}
+
+@ShellComponent
+class FlooShellComponent {
+    val log = LoggerFactory.getLogger(FlooShellComponent::class.java)
+    @ShellMethod("my first shell method")
+    fun first() {
+        log.info("working the pole")
+    }
+}
