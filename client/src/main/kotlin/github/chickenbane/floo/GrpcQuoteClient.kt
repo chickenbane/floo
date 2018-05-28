26fd5f604e38fa3f44343774e00291df335dd056
Joey T <thechickenbane@gmail.com>
2018-05-27 17:56:39 -0700

    Also build client Docker image

--- client/src/main/kotlin/github/chickenbane/floo/GrpcQuoteClient.kt
+++ client/src/main/kotlin/github/chickenbane/floo/GrpcQuoteClient.kt
@@ -3,26 +3,40 @@ package github.chickenbane.floo
 import github.chickenbane.proto.CreateQuoteRequest
 import github.chickenbane.proto.FindQuoteByIdRequest
 import github.chickenbane.proto.QuoteServiceGrpc
+import github.chickenbane.proto.QuoteServiceGrpc.QuoteServiceBlockingStub
+import github.saturnism.k8sapi.KubernetesNameResolverProvider
 import io.grpc.ManagedChannel
 import io.grpc.ManagedChannelBuilder
+import io.grpc.util.RoundRobinLoadBalancerFactory
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
 @Configuration
 class GrpcConfig {
     @Bean
-    fun channel(@Value("\${quote.server.host}") host: String,
-                @Value("\${quote.server.port}") port: Int): ManagedChannel =
+    @Profile("local")
+    fun localChannel(@Value("\${quote.server.host}") host: String,
+                     @Value("\${quote.server.port}") port: Int): ManagedChannel =
             ManagedChannelBuilder.forAddress(host, port).usePlaintext().build()
+
+    @Bean
+    @Profile("default")
+    fun channel(@Value("\${floo.service.target}") target: String): ManagedChannel =
+            ManagedChannelBuilder.forTarget(target)
+                    .nameResolverFactory(KubernetesNameResolverProvider())
+                    .loadBalancerFactory(RoundRobinLoadBalancerFactory.getInstance())
+                    .usePlaintext()
+                    .build()
 }
 
 @Component
-class GrpcQuoteClient(channel: ManagedChannel) {
+class GrpcQuoteClient(private val channel: ManagedChannel) {
     private val log = LoggerFactory.getLogger(GrpcQuoteClient::class.java)
-    private val stub = QuoteServiceGrpc.newBlockingStub(channel)
+    private fun stub(): QuoteServiceBlockingStub = QuoteServiceGrpc.newBlockingStub(channel)
 
     fun create(text: String, author: String): String {
         log.debug("create: text=$text author=$author")
@@ -30,7 +44,7 @@ class GrpcQuoteClient(channel: ManagedChannel) {
                 .setAuthor(author)
                 .setText(text)
                 .build()
-        val response = stub.create(request)
+        val response = stub().create(request)
         log.info("create: response id=${response.id}")
         return response.id
     }
@@ -38,7 +52,7 @@ class GrpcQuoteClient(channel: ManagedChannel) {
     fun findById(id: String): FindResponse {
         log.debug("findById: id=$id")
         val request = FindQuoteByIdRequest.newBuilder().setId(id).build()
-        val response = stub.findById(request)
+        val response = stub().findById(request)
         log.info("findById: response.text=${response.text} response.author=${response.author} response.server=${response.server}")
         return FindResponse(response.text, response.author, response.server)
     }
--- readme.md
+++ readme.md
@@ -1,4 +1,8 @@
 
+### Kubernetes API GRPC client load balancing
+
+https://github.com/saturnism/grpc-java-by-example
+
 ### Spring Shell
 
 Josh Long https://www.youtube.com/watch?v=h6nMjjxJWjk
